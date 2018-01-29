b8563a11db6d5b759cd85bf86e575983703542fc
nirobon <nirobon_saeku@hotmail.com>
2018-01-29 15:33:59 +0700

    edit

--- components/SectionHeader.js
+++ components/SectionHeader.js
@@ -1,38 +1,65 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import ReactNative, { View, Text } from 'react-native';
+'use strict';
 
-export default class SectionHeader extends React.PureComponent {
-  static propTypes = {
-    /**
-     * The id of the section
-     */
-    sectionId: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+import React, {
+  Component,
+  PropTypes
+} from 'react';
+import ReactNative, {
+  StyleSheet,
+  View,
+  Text
+} from 'react-native';
 
-    /**
-     * A component to render for each section item
-     */
-    component: PropTypes.func,
-
-    /**
-     * A function used to propagate the root nodes handle back to the parent
-     */
-    updateTag: PropTypes.func
-  };
+export default class SectionHeader extends Component {
 
   componentDidMount() {
-    this.props.updateTag &&
-      this.props.updateTag(ReactNative.findNodeHandle(this.refs.view), this.props.sectionId);
+    this.props.updateTag && this.props.updateTag(ReactNative.findNodeHandle(this.refs.view), this.props.sectionId);
   }
 
   render() {
     const SectionComponent = this.props.component;
-    const content = SectionComponent ? (
-      <SectionComponent {...this.props} />
-    ) : (
-      <Text>{this.props.title}</Text>
+    const content = SectionComponent ?
+      <SectionComponent {...this.props} /> :
+      <Text style={{fontFamily:'Montserrat-Regular',fontSize:22, marginLeft:20, marginTop:20, marginBottom:10}}>{this.props.title}</Text>;
+    return (
+      <View ref="view">
+        {content}
+      </View>
     );
-
-    return <View ref="view">{content}</View>;
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#f8f8f8',
+    borderTopWidth: 1,
+    borderTopColor: '#ececec',
+  },
+  text: {
+    fontWeight: '700',
+    paddingTop: 2,
+    paddingBottom: 2,
+    paddingLeft: 2,
+  }
+});
+
+SectionHeader.propTypes = {
+
+  /**
+   * The id of the section
+   */
+  sectionId: PropTypes.oneOfType([
+    PropTypes.number,
+    PropTypes.string
+  ]),
+
+  /**
+   * A component to render for each section item
+   */
+  component: PropTypes.func,
+
+  /**
+   * A function used to propagate the root nodes handle back to the parent
+   */
+  updateTag: PropTypes.func
+};
